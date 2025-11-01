a26a155a532ab979f683aac085c2dd6627b0c05f
maksimus <max.expertdev@gmail.com>
2025-11-01 17:52:32 +0600

    added picocli

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,8 @@ application {
 }
 
 dependencies {
+    implementation("info.picocli:picocli:4.7.7")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.7")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -23,4 +25,8 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.compileJava {
+    options.compilerArgs.add("-Aproject=${project.group}/${project.name}")
 }
