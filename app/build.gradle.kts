3ed606b7791305aaf589ed906b14c2dc91046fb2
maksimus <max.expertdev@gmail.com>
2025-11-01 17:41:54 +0600

    init

---
+++ app/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("application")
+    id("se.patrikerdes.use-latest-versions") version "0.2.19"
+    id("com.github.ben-manes.versions") version "0.53.0"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,8 @@
+package hexlet.code;
+
+public final class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
