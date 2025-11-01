434b291183580cfe80de029ec3d6e0eb16fe1b66
maksimus <max.expertdev@gmail.com>
2025-11-01 21:22:48 +0600

    checkstyle

--- app/src/main/java/hexlet/code/GenDiffCommand.java
+++ app/src/main/java/hexlet/code/GenDiffCommand.java
@@ -1,17 +1,20 @@
 package hexlet.code;
 
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-@Command(name = "gendiff", description = "Generate diff between two files")
+@Command(name = "gendiff", description = "Compares two configuration files and shows a difference.")
 public class GenDiffCommand implements Runnable {
-    @Parameters(index = "0", description = "First file")
-    private String file1;
-    @Parameters(index = "1", description = "Second file")
-    private String file2;
+    @Parameters(index = "0", description = "path to first file")
+    private String filepath1;
+    @Parameters(index = "1", description = "path to second file")
+    private String filepath2;
+    @Option(names = { "-f", "--format" }, defaultValue = "stylish", description = "output format [default: ${DEFAULT-VALUE}]")
+    private String format = "stylish";
 
     @Override
     public void run() {
-        System.out.println(Differ.generate(this.file1, this.file2));
+        System.out.println(Differ.generate(this.filepath1, this.filepath2));
     }
 }
