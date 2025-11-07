1d0edbbde654291c72d19523daabb872311c157d
maksimus <max.expertdev@gmail.com>
2025-11-07 17:08:15 +0600

    sonar.login is deprecated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ sonar {
         property("sonar.projectKey", "mtvru_java-project-71")
         property("sonar.organization", "mtvru")
         property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.login", System.getenv("SONAR_TOKEN"))
+        property("sonar.token", System.getenv("SONAR_TOKEN"))
         property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
     }
 }
